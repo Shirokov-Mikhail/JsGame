0d46d6fe30d82918f20c933754901d89698c23e2
Misha <m9339323029@gmail.com>
2026-03-10 23:22:05 +0700

    Add Asteroids

--- game.js
+++ game.js
@@ -26,6 +26,7 @@ let keys = {
     's': false,
     'd': false,
     'space': false,
+    'p': false,
 
 };
 let ticks = 0
@@ -88,8 +89,8 @@ class Invador {
             y_max: this.y + 40
         }
         image.onload = () => {
-            this.width = image.width;
-            this.height = image.height;
+            this.width = image.width * 1.5;
+            this.height = image.height * 1.5;
             this.image = image;
             this.cube = {
                 x_max: this.x + this.width,
@@ -151,38 +152,7 @@ class InvadorGroup {
         }
     }
 
-//    check_position(player_box) {
-//        let oneDel = false
-//
-////        this.invadors.forEach(invador => {
-////            this.index = this.invadors.indexOf(invador);
-////            if ((invador.x < player_box.x_max && player_box.x < invador.x )|| (invador.cube.x_max > player_box.x
-////                && invador.cube.x_max < player_box.x_max) ||
-////                (invador.x > player_box.x && invador.cube.x_max < player_box.x)) {
-////
-////                if (invador.cube.y_max > player_box.y && invador.y < player_box.y) {
-////                    oneDel = true;
-////
-////                }
-////            }
-////        })
-//         for (let i = 0; i < this.invadors.length; i++) {
-//            if ((this.invadors[i].x < player_box.x_max && player_box.x < this.invadors[i].x )|| (this.invadors[i].cube.x_max > player_box.x
-//                && this.invadors[i].cube.x_max < player_box.x_max) ||
-//                (this.invadors[i].x > player_box.x && this.invadors[i].cube.x_max < player_box.x)) {
-//                if (this.invadors[i].cube.y_max > player_box.y && this.invadors[i].y < player_box.y) {
-//                    this.invadors.splice(i, 1)
-//                    oneDel = true;
-//
-//        }}}
-//
-//       if (oneDel) {
-//           this.damage = 1;
-//      } else {
-//            oneDel = false;
-//            this.damage = 0;
-//    }
-//    }
+
 check_position(player_box) {
     let oneDel = false;
     this.damage = 0;
@@ -194,26 +164,127 @@ check_position(player_box) {
             player_box.y_max > invador.y) {
             this.invadors.splice(i, 1);
             oneDel = true;
+            this.damage = 1;
         }
     }
 
-    if (oneDel) {
-        this.damage = 1;
-    }
 }
     clear() {
         this.invadors = [];
     }
 }
 
+class Asteroid{
+    constructor(x) {
+        this.x = x;
+        this.y = 100 * Math.random();
+        this.vel = {
+            x: 0,
+            y: 2
+        }
+        this.start = false
+        let image = new Image();
+        image.src = "asteroid.png";
+        this.cube = {
+            x_max: this.x + 40,
+            y_max: this.y + 40
+        }
+        image.onload = () => {
+            this.width = image.width * 0.1;
+            this.height = image.height * 0.1;
+            this.image = image;
+            this.cube = {
+                x_max: this.x + this.width,
+                y_max: this.y + this.height
+            }
+            this.start = true
+        }
+
+    }
+
+    update() {
+        if (this.start){
+            this.x += this.vel.x;
+            this.y += this.vel.y;
+            this.cube.x_max = this.x + this.width;
+            this.cube.y_max = this.y + this.height;
+
+            ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        }
+        else {
+            this.cube.x_max = this.x + 40;
+            this.cube.y_max = this.y + 40;
+        }
+
+    }
+}
+
+class AsteroidGroup {
+    constructor() {
+        this.asteroids = [];
+        this.delete = false
+        this.len = this.asteroids.length;
+        this.damage = 0;
+    }
+
+    create(length) {
+        for (let i = 0; i < length; i++) {
+
+            this.asteroids.push(new Asteroid(Math.floor(Math.random() * 1921)));
+        }
+
+        this.len = this.asteroids.length;
+    }
+
+    update() {
+        this.asteroids.forEach(asteroid => {
+            asteroid.update()
+            if (asteroid.y > canvas.height - asteroid.height) {
+                this.delete = true;
+
+            }
+        })
+        if (this.delete) {
+            let len = this.asteroids.length;
+
+            this.asteroids = []
+            this.create(this.len)
+            this.delete = false
+        }
+    }
+
+
+    check_position(player_box) {
+        let oneDel = false;
+        this.damage = 0;
+        for (let i = this.asteroids.length - 1; i >= 0; i--) {
+            let asteroid = this.asteroids[i];
+            if (player_box.x < asteroid.cube.x_max &&
+                player_box.x_max > asteroid.x &&
+                player_box.y < asteroid.cube.y_max &&
+                player_box.y_max > asteroid.y) {
+                this.asteroids.splice(i, 1);
+                oneDel = true;
+            }
+        }
+        if (oneDel) {
+            this.damage = 1;
+        }
+    }
+    clear() {
+        this.asteroids = [];
+    }
+}
+
 class Fire {
-    constructor(x, y) {
+    constructor(x, y, vel, color='red') {
         this.x = x;
         this.y = y;
         this.vel = {
             x: 0,
-            y: -5
+            y: vel
         }
+        this.color = color;
         this.cube = {
             x_max: this.x + 20,
             y_max: this.y + 20,
@@ -226,7 +297,7 @@ class Fire {
     update() {
         this.x += this.vel.x;
         this.y += this.vel.y;
-        ctx.fillStyle = "red";
+        ctx.fillStyle =  this.color;
         ctx.fillRect(this.x, this.y, 10, 10)
     }
 }
@@ -369,6 +440,13 @@ document.addEventListener("keydown", (event) => {
         case 'Space':
             keys.space = true;
             break
+        case 'KeyP':
+            if (keys.p === false){
+                keys.p = true;
+            }
+            else {
+                keys.p = false;
+            }
     }
 })
 
@@ -394,99 +472,130 @@ document.addEventListener("keyup", (event) => {
 
     }
 })
+
+
 const fuelbox = [new FuelKit()]
 const helpbox = [new HelpKit()]
+
 const player = new Player();
+
 const invadors = [new InvadorGroup()];
+const asteroids = [new AsteroidGroup()];
+
 const fire = [];
-invadors[invadors.length - 1].create(48);
+
+asteroids[0].create(5)
+invadors[invadors.length - 1].create(5);
 
 function draw() {
     if (start_game){
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        player.update();
-
-        for (let i = 0; i < fire.length; i++) {
-            fire[i].update();
-            if (fire[i].y < 0){
-                fire.splice(i, 1)
-            } 
-        }
-        for (let i = 0; i < invadors.length; i++) {
-            
-            for (let j = 0; j < fire.length; j++){
-                invadors[i].check_position(fire[j].cube)
-                
+        console.log(keys.p)
+        if (keys.p === false){
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            player.update();
+            // Стрельба
+            for (let i = 0; i < fire.length; i++) {
+                fire[i].update();
+                if (fire[i].y < 0){
+                    fire.splice(i, 1)
+                }
             }
-            invadors[i].check_position(player.cube);
-            hp_val -= invadors[i].damage
-            invadors[i].update()
-        }
+            //Враги
+            for (let i = 0; i < invadors.length; i++) {
 
-        
-        for (let i = 0; i < helpbox.length; i++) {
-            helpbox[i].update()
-            helpbox[i].check_position(player.cube);
-            if (helpbox[i].help != 0 & hp_val + helpbox[i].help <= 100){
-                hp_val += helpbox[i].help
+                for (let j = 0; j < fire.length; j++){
+                    invadors[i].check_position(fire[j].cube)
+                    if (invadors[i].damage !== 0){
+                        fire.splice(j, 1)
+                    }
+                }
+                invadors[i].check_position(player.cube);
+                hp_val -= invadors[i].damage
+                invadors[i].update()
             }
-            if (helpbox[i].oneDel){
+            //Астероиды
+            for (let i = 0; i < asteroids.length; i++) {
+                for (let j = 0; j < fire.length; j++){
+                    asteroids[i].check_position(fire[j].cube)
+                    if (asteroids[i].damage !== 0){
+                        fire.splice(j, 1)
+                    }
+                }
+                asteroids[i].check_position(player.cube);
+                hp_val -= asteroids[i].damage
+                asteroids[i].update()
+            }
+            //Аптечка
+            for (let i = 0; i < helpbox.length; i++) {
+                helpbox[i].update()
+                helpbox[i].check_position(player.cube);
+                if (helpbox[i].help != 0 & hp_val + helpbox[i].help <= 100){
+                    hp_val += helpbox[i].help
+                }
+                if (helpbox[i].oneDel){
+                    helpbox.splice(0, 1);
+                }
+            }
+            //Топливо
+            for (let i = 0; i < fuelbox.length; i++) {
+                fuelbox[i].update()
+                fuelbox[i].check_position(player.cube);
+                if (fuelbox[i].fuel != 0 & fuel_val + fuelbox[i].fuel <= 100){
+                    fuel_val += fuelbox[i].fuel
+                }
+                if (fuelbox[i].oneDel){
+                    fuelbox.splice(0, 1);
+                }
+            }
+            //клавиши
+            if (keys.a & player.x > 0) {
+                player.vel.x = -1;
+                player.vel.y = 0;
+            } else if (keys.w & player.y > 0) {
+                player.vel.x = 0;
+                player.vel.y = -1;
+            } else if (keys.s & player.y + player.height < canvas.height) {
+                player.vel.x = 0;
+                player.vel.y = 1;
+            } else if (keys.d & player.x + player.width < canvas.width) {
+                player.vel.x = 1;
+                player.vel.y = 0;
+            } else {
+                player.vel.x = 0;
+                player.vel.y = 0;
+            }
+            if (keys.space) {
+                fire.push(new Fire(player.fire_pos, player.y, -5));
+            }
+
+            //время
+            if (ticks >= 60 * 24) {
+                ticks = 0;
+                invadors.push(new InvadorGroup());
+                invadors[invadors.length - 1].create(5);
+            }
+            if (ticks % 60*4 === 0) {
+                fuel_val -= 1
+                money_val += 1
+            }
+            if (ticks === 60*8) {
                 helpbox.splice(0, 1);
-            }
-        }
-        for (let i = 0; i < fuelbox.length; i++) {
-            fuelbox[i].update()
-            fuelbox[i].check_position(player.cube);
-            if (fuelbox[i].fuel != 0 & fuel_val + fuelbox[i].fuel <= 100){
-                fuel_val += fuelbox[i].fuel
-            }
-            if (fuelbox[i].oneDel){
+                helpbox.push(new HelpKit());
                 fuelbox.splice(0, 1);
+                fuelbox.push(new FuelKit());
             }
+            //Здоровье и топливо
+            if (hp_val < 0 || fuel_val < 0){
+                hp_val = 0
+                start_game = false
+                end_game = true
+            }
+            money.textContent = `${money_val} Монет`
+            fuel.textContent = `${fuel_val}%`
+            hp.textContent = `${hp_val}%`;
+            ticks++;
+
         }
-        if (keys.a & player.x > 0) {
-            player.vel.x = -1;
-            player.vel.y = 0;
-        } else if (keys.w & player.y > 0) {
-            player.vel.x = 0;
-            player.vel.y = -1;
-        } else if (keys.s & player.y + player.height < canvas.height) {
-            player.vel.x = 0;
-            player.vel.y = 1;
-        } else if (keys.d & player.x + player.width < canvas.width) {
-            player.vel.x = 1;
-            player.vel.y = 0;
-        } else {
-            player.vel.x = 0;
-            player.vel.y = 0;
-        }
-        if (keys.space) {
-            fire.push(new Fire(player.fire_pos, player.y));
-        }
-        if (ticks >= 60 * 8) {
-            helpbox.splice(0, 1);
-            helpbox.push(new HelpKit());
-            fuelbox.splice(0, 1);
-            fuelbox.push(new FuelKit());
-            ticks = 0;
-           
-            invadors.push(new InvadorGroup());
-            invadors[invadors.length - 1].create(48);
-        }
-        
-        if (ticks % 60*4 === 0) {
-            fuel_val -= 1
-            money_val += 1
-        }
-        if (hp_val < 0 || fuel_val < 0){
-            hp_val = 0
-            start_game = false
-            end_game = true
-        }
-        money.textContent = `${money_val} Монет`
-        fuel.textContent = `${fuel_val}%`
-        hp.textContent = `${hp_val}%`;
-        ticks++;
         window.requestAnimationFrame(draw);
     }else {
         if (end_game){
