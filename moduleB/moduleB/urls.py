b9db79ea45cf7a3ddfc394ba2d674e512eb9a35e
Misha <m9339323029@gmail.com>
2026-03-26 13:39:54 +0700

    add my sql connect

--- moduleB/moduleB/urls.py
+++ moduleB/moduleB/urls.py
@@ -2,7 +2,7 @@
 URL configuration for moduleB project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-import vievs
+from . import vievs
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', )
+    path('', vievs.index)
 ]
+
--- moduleB/moduleB/vievs.py
+++ moduleB/moduleB/vievs.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect, render
+from django.views.decorators.csrf import ensure_csrf_cookie
+from . import mysql_connect
 
 def index(request):
-    data = {}
-    return render('../static/..', context=data)
+    data = {
+        'email_error': ['white', ''],
+        'password_error':['white', '']}
+    if request.method == 'POST':
+        mail = request.POST.get('email')
+        password = request.POST.get('password')
+        db = mysql_connect.Db()
+        if db.auth(mail, password):
+            return redirect('')
+        data['email_error'] = ['red', 'Не правильный Email']
+        data['password_error'] = ['red', 'Не правильный Пароль']
+    return render(request,'index.html', context=data)
 
+def books(request):
+    data = {
+        '': []
+    }
